2778685e84a50c46472598027525f95416b74367
Miguel Pereira <miguelpereira1242@gmail.com>
2022-12-02 00:04:20 -0400

    agregamos la urls de entrega

--- entrega/urls.py
+++ entrega/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from familia.views import index,  monstrar_familiar
 urlpatterns = [
       path('admin/', admin.site.urls),
-      path('familia/', monstrar_familiar), # nueva vista
+      path('familia/', monstrar_familiar), 
   ]
  
