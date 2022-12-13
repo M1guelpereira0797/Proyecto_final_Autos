2dc8cc54dfc8bec7cd14a4b85d0d77920143501b
Miguel Pereira <miguelpereira1242@gmail.com>
2022-12-13 19:14:02 -0300

    clases al models

--- familia/models.py
+++ familia/models.py
@@ -6,3 +6,19 @@ class familia(models.Model):
     numero_pasaporte = models.IntegerField()
     def __str__(self):
       return f"{self.nombre}, {self.direccion},{self.numero_pasaporte}, {self.id}"
+
+class Vehiculos(models.Model):
+    nombre_vehiculo= models.CharField(max_length=100)
+    
+    marca_vehiculo = models.CharField(max_length=200)
+    numero_de_vehiculo = models.IntegerField()
+    def __str__(self):
+      return f"{self.nombre_vehiculo}, {self.marca_vehiculo},{self.numero_de_vehiculo}"
+
+class Equipo_de_futbol(models.Model):
+    nombre_equipo= models.CharField(max_length=100)
+    
+    marca_deportiva = models.CharField(max_length=200)
+    numero_de_jugador_favorito = models.IntegerField()
+    def __str__(self):
+      return f"{self.nombre_equipo}, {self.marca_deportiva},{self.numero_de_jugador_favorito}"
